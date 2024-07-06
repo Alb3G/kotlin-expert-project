6a7efff454142ed25297a056bd84f20ab32e1be9
Alberto <guzmanmorenoalberto1998@gmail.com>
2024-07-06 17:36:08 +0200

    Finished app now works with the persisting functionality that the server provide us

--- src/main/kotlin/dev/alb3g/remote/NotesRepository.kt
+++ src/main/kotlin/dev/alb3g/remote/NotesRepository.kt
@@ -1,27 +1,16 @@
 package dev.alb3g.remote
 
-import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
-import dev.alb3g.database.AppDatabase
 import dev.alb3g.model.Note
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 
-
 private const val NOTES_URL = "http://localhost:8080/notes"
 
 object NotesRepository {
-    private val notesDb = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).let {
-        AppDatabase.Schema.create(it)
-        AppDatabase(it)
-    }
-
     suspend fun getAll(): List<Note> = notesClient.request(NOTES_URL).body()
 
-    suspend fun getById(id: Long): Note {
-        val response = notesClient.request("$NOTES_URL/$id")
-        return response.body()
-    }
+    suspend fun getById(id: Long): Note = notesClient.request("$NOTES_URL/$id").body()
 
     suspend fun save(note: Note) {
         notesClient.post(NOTES_URL) {
@@ -37,7 +26,5 @@ object NotesRepository {
         }
     }
 
-    suspend fun delete(note: Note) {
-        notesClient.delete("$NOTES_URL/${note.id}")
-    }
+    suspend fun delete(note: Note) = notesClient.delete("$NOTES_URL/${note.id}")
 }
